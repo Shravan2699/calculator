e8921d272f9de757cea348e76045ae51a4075037
Shravan_Kini <kinishravan4@gmail.com>
2022-07-06 23:00:00 +0530

    The Calulator Project is complete
    Have skipped the optional part of binding keyboard buttons with the calculator
     keys
    
    The '.'(decimal point key) also needs to be fixed.Currently it adds multiple
    decimal points to a typed number.Need to find a way to check if there are any
    existing decimal points in the inputs.If there are none,only then add decimal
    points.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+//Can bind the keyboard keys with calculator keys,will do this some other time.This took a long time.NEED TO MOVE AHEAD NOW:))
+
+
 let display = document.querySelector('.screen');
 let inScreen = document.createElement('div');
 let childDivs = document.querySelector('.container').getElementsByTagName('div');
 inScreen.innerText = '0'
+inScreen.style.fontSize = '2rem'
 display.append(inScreen)
 
 let arr =[]
 let numbers = []
-// numbers[0] = 0;
 let sign;
 let num = ''
 let currentValue = 0;
@@ -19,7 +22,6 @@ console.log(`Numbers[0] before function is run: ${numbers[0]}`)
 
 
 function operations(num1,num2,operator){
-    // const newArr = parseFloat(numbers[0])
     if (sign === '+'){
         num1+=parseFloat(num2);
         return num1;
@@ -38,8 +40,20 @@ function operations(num1,num2,operator){
 
 for (let j=0;j < arr.length;j++){
     arr[j].addEventListener('click',()=>{
+    if (arr[j].className === '.'){
 
-    if (arr[j].className === '+'||arr[j].className === '-'||arr[j].className === '*'||arr[j].className === '/'){
+    }else if (arr[j].className === 'bak'){
+        console.log('Hiiii');
+        num = num.slice(0,num.length-1)
+        console.log(numbers[0])
+        inScreen.innerText = num
+        if (numbers[0] !== undefined){
+            let strAnswer = numbers[0].toString();
+            strAnswer = strAnswer.slice(0,strAnswer.length-1);
+            inScreen.innerText = strAnswer;
+            numbers[0] = parseFloat(strAnswer)
+        }
+    } else if (arr[j].className === '+'||arr[j].className === '-'||arr[j].className === '*'||arr[j].className === '/'){
         inScreen.innerText = '';
         console.log(numbers)
         numbers.push(num)
@@ -49,46 +63,48 @@ for (let j=0;j < arr.length;j++){
         inScreen.innerText = sign
         // console.log(numbers)
         num = '';
-        } else if (arr[j].className === '='){
-            numbers[0] = (operations(parseFloat(numbers[0]),currentValue,sign));
-            console.log(numbers)
-            console.log(`I am Num[0]: ${numbers[0]}`)
-            inScreen.innerText = numbers[0];
-            // console.log(operations(parseFloat(numbers[0],currentValue,sign)));
-        } else if (arr[j].className === 'clear'){
+    } else if (arr[j].className === '='){
+        numbers[0] = (operations(parseFloat(numbers[0]),currentValue,sign));
+        console.log(numbers)
+        console.log(`I am Num[0]: ${numbers[0]}`)
+        inScreen.innerText = numbers[0];
+        if (arr[j].className === 'bak'){
+            backSpace(numbers[0]);
+        }
+    } else if (arr[j].className === 'ON/OFF'){
+        numbers = [];
+        num = '';
+        inScreen.innerText = '';
+        currentValue = 0;
+    }else if (arr[j].className === 'clear'){
+        if (inScreen.innerText!== ''){
             numbers = [];
+            inScreen.innerText = '0';
             num = '';
+        } else {
             inScreen.innerText = '';
-            currentValue = 0;
-        }else if (arr[j].className === 'ON/OFF'){
-            if (inScreen.innerText!== ''){
-                numbers = [];
-                inScreen.innerText = '0';
-                num = '';
-            } else {
-                inScreen.innerText = '';
-                num = '';
-            }  
-        } else if( arr[j].className === 'negativeSign'){
-            if (numbers[0] > 0){
-                num = -num;
-                inScreen.innerText = `${num}`
-                // numbers[0] = -numbers[0];
-            } else {
-                // numbers[0] = -1 * numbers[0];
-                num = -1 * num;
-                inScreen.innerText = `${num}`
+            num = '';
             }
-        // }else if (arr[j].className = 'bak'){
-        //     num = num.slice(0,num.length-1)
-        }else {
-            num += arr[j].className;
-            currentValue = parseFloat(num)
-            inScreen.innerText =  num;
-            console.log(currentValue)
-            }
-        })
+    } else if( arr[j].className === 'negativeSign'){
+        if (numbers[0] > 0){
+            num = -num;
+            inScreen.innerText = `${num}`
+            numbers[0] = -numbers[0];
+        } else {
+            numbers[0] = -1 * numbers[0];
+            num = -1 * num;
+            inScreen.innerText = `${num}`
+        }
+    } else {
+        num += arr[j].className;
+        currentValue = parseFloat(num)
+        inScreen.innerText =  num;
+        console.log(currentValue)
+        }
+    }
+    )
 }
 
-//Need to work on the backspace key
-//Rest all is working fine:)
+
+let abc = 'STRING';
+console.log(abc.slice(0,abc.length-1))
